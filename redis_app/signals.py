468128030e638b5043c730ac8093c76d9311363e
Ananthu P <ananthup303@gmail.com>
2026-01-20 08:35:54 +0000

    Cache posts

--- redis_app/signals.py
+++ redis_app/signals.py
@@ -1,5 +1,5 @@
 from django.core.cache import cache
-from django.db.models.signals import post_save, m2m_changed
+from django.db.models.signals import post_save, post_delete, m2m_changed
 from django.dispatch import receiver
 
 from .models import CustomUser, Post
@@ -15,6 +15,10 @@ def invalidate_user_me_cache(sender, instance, **kwargs):
 def invalidate_post_cache(sender, instance, **kwargs):
     cache_key = f"user_posts_{instance.user_id}"
     cache.delete(cache_key)
+
+@receiver(post_delete, sender=Post)
+def invalidate_post_cache_on_delete(sender, instance, **kwargs):
+    cache.delete(f"user_posts_{instance.user_id}")
 
 
 @receiver(m2m_changed, sender=Post.likes.through)
